89abb0d72b2cdd2f0fc4559f1f1f251acb1334a3
Jdan2002 <sjsfm.1912@gmail.com>
2025-11-19 19:56:10 -0300

    Agregué los HTML de Servicio, Equipo y Contacto pero no  tienen contenido.

--- core/views.py
+++ core/views.py
@@ -3,3 +3,15 @@ from django.views.generic import TemplateView
 
 class LandingView(TemplateView):
     template_name = "core/landing.html"
+
+
+class ServicesView(TemplateView):
+    template_name = "core/servicios.html"
+
+
+class TeamView(TemplateView):
+    template_name = "core/equipo.html"
+
+
+class ContactView(TemplateView):
+    template_name = "core/contacto.html"
